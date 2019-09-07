64c1d969a6f38bfd697e09a9ee41dc4a8c4d8407
Ray Riney <rayfordriney@gmail.com>
2019-09-07 11:20:57 -0700

    work in  progress

---
+++ app.js
@@ -0,0 +1,20 @@
+
+//.document.getElementById('title')  = //jquery
+//$("#title").html())
+//jqery
+//$('#title').text('hotdog')
+
+c//onts arr = [1,2,3,4,5,6,7,8,9,10]
+//arr.forEach(function(x) {
+    //const numBtn = $('<button>')
+    //numBtn.text(x)
+
+
+    //document.getElemtById('btn').addEventListener('click'), function(){
+
+    
+
+
+
+
+}
---
+++ java.js
@@ -0,0 +1,8 @@
+const getMax = function(a,b){
+    if(a>b){
+    console.log(a)
+    else if (b>a){
+     console.log(b)
+           else{
+   console.log('both numbers are equal')
+   
